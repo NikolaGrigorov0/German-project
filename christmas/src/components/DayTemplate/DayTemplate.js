492900afd3b55049173ab52a57703d2d5ddc2046
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2023-12-05 23:10:18 +0200

    page template done

--- christmas/src/components/DayTemplate/DayTemplate.js
+++ christmas/src/components/DayTemplate/DayTemplate.js
@@ -1,28 +1,47 @@
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./DayTemplate.css";
 
 
-export const DayPage = ({ dayNumber, germanWord, audioSrc, facts }) => {
-    const navigate = useNavigate()
-    const goToHomePage = () => {
+export const DayPage = ({ dayNumber, germanWord, audioSrc, facts, imageUrl }) => {
+  const navigate = useNavigate();
+  const goToHomePage = () => {
     navigate(`/`);
-  }
+  };
+  const audio = process.env.PUBLIC_URL + '/first-page.mp3';
 
   return (
     <div>
       <div className="header">
-        <button onClick={() => goToHomePage()}>Home</button>
-        <div className="day-number ">Day {1}</div>
+        <button className="text-xl" onClick={() => goToHomePage()}>
+          Zurück
+        </button>
+        <div className="number-div absolute w-60 flex items-center justify-center m-6 rounded-full text-center h-60 top-0 right-0 shadow-md p-4 text-white">
+          <span className="text-9xl text-black font-bold">{1}</span>{" "}
+        </div>
       </div>
 
-      <div className="content">
-        <h1 className="word">{"random word"}</h1>
-        <audio controls>
-          <source src={audioSrc} type="audio/mp3" />
-          Your browser does not support the audio element.
-        </audio>
-        <p className="fact">{"random facts"}</p>
+      <div className="content w-full">
+        <h1 className="word text-center font-sans">
+          {"random word-translation"}
+        </h1>
+      </div>
+      <div
+        id="info"
+        className="absolute w-full flex justify-center items-center top-1/3 h-80"
+      >
+        <div className="p-10">
+          <p className="fact flex p-5">{"random facts"}</p>
+          <audio controls>
+            <source className="flex" src={audio} type="audio/mp3" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <img
+          src="https://thehill.com/wp-content/uploads/sites/2/2022/12/CA_christmas_12152022istock.jpg"
+          alt="Christmas"
+          className="w-full h-auto rounded-md shadow-md border-2 border-gray-300"
+        />
       </div>
     </div>
   );
 };
-
